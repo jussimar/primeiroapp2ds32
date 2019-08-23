17c5d5e67a02f497b20be18c2c9a1729d239b49c
jussimar <jussimar@jussimarleal.com.br>
2019-08-24 07:16:44 +0900

    camera e localização

--- www/components/acao.js
+++ www/components/acao.js
@@ -21,3 +21,73 @@ $(document).on("click","#alerta",function(){
     navigator.notification.confirm("Escolha uma opção!",retorno2,"Titulo",['Sim','Não']);
 });
 
+$(document).on("click","#vibrar",function(){
+    navigator.vibrate(3000);
+});
+
+$(document).on("click","#codigo",function(){
+   cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          if(result.text == 888888){
+            $(location).attr("href","produto1.html");
+          }
+      },
+      function (error) {
+          alert("Falha no scanner: " + error);
+      },
+      {
+          preferFrontCamera : false, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: false, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417,CODE_39", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+   );
+});
+
+$(document).on("click","#camera",function(){
+    navigator.camera.getPicture(onSuccess, onFail, { 
+    quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI,
+    correctOrientation:true,
+    saveToPhotoAlbum:true
+  });
+
+  function onSuccess(imageURI) {
+      var image = document.getElementById('imagem');
+      image.src = imageURI;
+  }
+
+  function onFail(message) {
+      navigator.notification.alert('Erro ao capturar imagem: ' + message);
+  }
+});
+
+$(document).on("click","#local",function(){
+   var onSuccess = function(position) {
+        navigator.notification.alert(
+              'Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+    };
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        navigator.notification.alert('codigo: '    + error.code    + '\n' +
+              'menssagem: ' + error.message + '\n');
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+});
